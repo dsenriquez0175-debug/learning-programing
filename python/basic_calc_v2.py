32b723f1c592c0ea638c99967290968b81a83f31
Daniel E. <danielstevenenriquez@gmail.com>
2026-04-07 09:29:40 -0500

    java

--- python/basic_calc_v2.py
+++ python/basic_calc_v2.py
@@ -4,14 +4,21 @@ In python, it is not neccesary or mandatory to declare
 variables or constants, but you can initialize them.
 '''
 # Assign or initialize variables or constants
+
 num1 = 0
+print("number 1 type: ", type (mun1))
 num2 = 0
+print("number 2 type: ", type (num2))
+
+num 1 = 1.5
+print("Now, numer 1 type ",type (num1))
 
 # Inputs
 print(please, enter first number: "")
 num1= input()
 print(please. enter second number: "")
 num2= input()
+
 #processes
 add = num1 + num2
 #subs = num1 - num2
