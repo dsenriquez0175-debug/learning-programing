8db9b234faad0e94b5c614a18f29ba8374b3d76a
Daniel E. <danielstevenenriquez@gmail.com>
2026-04-14 09:29:09 -0500

    basic_calc_v3

--- python/basic_calc_v3.py
+++ python/basic_calc_v3.py
@@ -3,14 +3,15 @@ import os
 # Main menu function
 def main_menu():
 
-    print("#### MAIN MENU ####")
-    print("1. addition")
-    print("2. substraction")
-    print("3. multiplication")
-    print("4. division")
-    print("5. average")
-    print("6. all options")
+   print("#### MAIN MENU ####")
+   print("1. addition")
+   print("2. substraction")
+   print("3. multiplication")
+   print("4. division")
+   print("5. average")
+   print("6. all options")
 
+os.system('clear')
 # Inputs
 n1= float(input("Enter first number: "))
 n2= float(input("Enter second number: "))
@@ -18,39 +19,38 @@ n2= float(input("Enter second number: "))
 main_menu()
 opt = int(input("Enter any option: "))
 
- if (opt == 1):
-    add = n1 + n2
-    print(f"addition is: {add}") # print("addition is: ", add)
+if (opt == 1):
+   add = n1 + n2
+   print(f"addition is: {add}") # print("addition is: ", add)
 
-
- if (opt == 2):
-    subs = n1 - n2
-    print(f"substraction is: {subs}") # print("addition is: ", add)
+if (opt == 2):
+   subs = n1 - n2
+   print(f"substraction is: {subs}") # print("substraction is: ", subs)
 
 if (opt == 3):
-    mult = n1 * n2
-    print(f"multiplication is: {mult}") # print("addition is: ", add)
+   mult = n1 * n2
+   print(f"multiplication is: {mult}") # print("multiplication is: ", mult)
 
- if (opt == 4):
-    div = n1 / n2
-    print(f"division is: {div}") # print("addition is: ", add)
+if (opt == 4):
+   div = n1 / n2
+   print(f"division is: {div}") # print("division is: ", div)
 
- if (opt == 5):
-    avg = (n1 + n2) / 2
-    print(f"average is: {avg}") # print("addition is: ", add)
+if (opt == 5):
+   avg = (n1 + n2) / 2
+   print(f"average is: {avg}") # print("average is: ", avg)
 
- if (opt == 6):
-    add = n1 + n2
-    subs = n1 - n2
-    mult = n1 * n2
-    div = n1 / n2
-    avg = (n1 + n2) / 2
+if (opt == 6):
+   add = n1 + n2
+   subs = n1 - n2
+   mult = n1 * n2
+   div = n1 / n2
+   avg = (n1 + n2) / 2
 
 
 
-    print(f"addition is: {add}") 
-    print(f"substraction is: {add}")
-    print(f"multiplication is: {add}")
-    print(f"addition is: {add}")
-    print(f"addition is: {add}")
+   print(f"addition is: {add}") 
+   print(f"substraction is: {subs}")
+   print(f"multiplication is: {mult}")
+   print(f"division is: {div}")
+   print(f"average is: {avg}")
 
